9b14407cfb5d7e609f21995008a1440389b2f47e
Aron Lawrence <aron_lawrence@premierinc.com>
2022-04-28 16:05:56 -0600

    wip

---
+++ docs/guides/install.md
@@ -0,0 +1,12 @@
+# Installation
+
+To install the `randomly` package, clone the repository to your local maching,
+go to the repo, and use:
+
+```bash
+python3 -m pip install -e .
+```
+
+```{tip}
+Ensure pip is also up to date, to allow for installation using setup.cfg
+```
--- randomly/facts.py
+++ randomly/facts.py
@@ -1,18 +1,29 @@
+from typing import Union, Literal
+
 import requests
 from requests.exceptions import RequestException
 
 
-def generate_random_fact(output_format, language):
+def generate_random_fact(output_format: Literal["html", "json", "txt", "md"],
+                         language: Literal["en", "de"]) -> Union[str, dict]:
+    """
+    Calls uselessfacts api and returns response
+    :param output_format: str, should be one of: "html", "json", "txt", "md"
+    :param language: str, should be "en" or "de"
+    :return:
+    """
+    # sanity checks for inputs
     if language not in {"en", "de"}:
         raise ValueError(f"{language} is not supported.")
 
     if output_format not in {"html", "json", "txt", "md"}:
         raise ValueError(f"{output_format} is not supported.")
 
-    response = requests.get(
+    response: requests.Response = requests.get(
         f"https://uselessfacts.jsph.pl/random.{output_format}?language={language}"
     )
 
+    # response handler
     if response.status_code == 200:
         if output_format == "json":
             fact = response.json()
